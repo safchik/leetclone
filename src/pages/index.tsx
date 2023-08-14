5d6c570b17474d0d04eb1aad7cda17c3986c88cd
safchik <safchik9@gmail.com>
2023-08-14 15:32:36 +0100

    basic setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}>
+    <main>
+      <h1>Hello</h1>
     </main>
   )
 }
