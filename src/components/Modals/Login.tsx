86059a1d2fbfc2f3bd01f3ca6171d66af9c1c167
safchik <safchik9@gmail.com>
2023-08-15 13:47:34 +0100

    auth state modal added, firebase connected with registering new user with some errors handlers

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import { authModalState } from '@/atoms/authModalAtom';
+import { auth } from '@/firebase/firebase';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSetRecoilState } from 'recoil';
 
 type LoginProps = {
 
 };
 
 const Login: React.FC<LoginProps> = () => {
+    const setAuthModalState = useSetRecoilState(authModalState);
+    const handleClick = (type: 'login' | 'register' | 'forgotPassword') => {
+        setAuthModalState((prev) => ({ ...prev, type }));
+    };
+    const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+    const router = useRouter();
+    const [inputs, setInputs] = useState({ email: "", password: "" });
+    const handleInputChange = (e: React.ChangeEvent<HTMLImageElement>) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!inputs.email || !inputs.password) return alert("Please fill all fields");
+        try {
+            const newUser = await signInWithEmailAndPassword(inputs.email, inputs.password);
+            if (!newUser) return;
+            router.push("/");
+        } catch (error: any) {
+            alert(error.message);
+        }
+    }
 
+    useEffect(() => {
+        if (error) alert(error.message);
+    }, [error]);
 
-    return <form className="space-y-6 px-6 pb-4" >
+    return <form className="space-y-6 px-6 pb-4" onSubmit={handleLogin}>
         <h3 className="text-xl font-medium text-white">Sign in to LeetClone</h3>
         <div>
             <label htmlFor="email" className="text-sm font-medium block mb-2 text-gray-300">
                 Your Email
             </label>
-            <input type="email" name="email" id="email" className='
+            <input onchange={handleInputChange} type="email" name="email" id="email" className='
             border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
             bg-gray-600 border-gray-400 placeholder-gray-400 text-white'
                 placeholder='name@company.com' />
@@ -22,7 +51,7 @@ const Login: React.FC<LoginProps> = () => {
             <label htmlFor="password" className="text-sm font-medium block mb-2 text-gray-300">
                 Your Password
             </label>
-            <input type="password" name="password" id="password" className='
+            <input onchange={handleInputChange} type="password" name="password" id="password" className='
             border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
             bg-gray-600 border-gray-400 placeholder-gray-400 text-white'
                 placeholder='********' />
@@ -30,7 +59,7 @@ const Login: React.FC<LoginProps> = () => {
         <button type='submit' className='w-full text-white focus:ring-blue-300 font-medium rounded-lg
             text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s
         '>
-            Log In
+            {loading ? "Loading..." : "Log In"}
         </button>
         <button className='flex w-full justify-end' onClick={() => handleClick('forgotPassword')}>
             <a href='#' className='text-sm block text-brand-orange hover:underline w-full text-right'>
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,12 +1,20 @@
+import { authModalState } from '@/atoms/authModalAtom';
 import AuthModal from '@/components/Modals/AuthModal';
 import Navbar from '@/components/Navbar/Navbar';
-import React from 'react';
+
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+
+import { useRecoilValue } from 'recoil';
 
 type AuthPageProps = {
 
 };
 
 const AuthPage: React.FC<AuthPageProps> = () => {
+    const authModal = useRecoilValue(authModalState);
+
+
 
     return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
         <div className="max-w-7xl mx-auto">
@@ -14,7 +22,7 @@ const AuthPage: React.FC<AuthPageProps> = () => {
             <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
                 <img src='/hero.png' alt='Hero img' />
             </div>
-            <AuthModal />
+            {authModal.isOpen && <AuthModal />}
         </div>
     </div>
 }
