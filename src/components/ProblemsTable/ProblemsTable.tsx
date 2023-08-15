cef7db98dc3ce512a8873555dc85b283069a1bb9
safchik <safchik9@gmail.com>
2023-08-15 18:28:47 +0100

    problem table and topbar functionality added

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -73,23 +73,12 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
             </tbody>
             {youtubePlayer.isOpen && (
                 <tfoot className='fixed top-0 left-0 h-screen w-screen flex items-center justify-center'>
-                    <div
-                        className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute'
-                        onClick={closeModal}
-                    ></div>
+                    <div className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute' onClick={closeModal} ></div>
                     <div className='w-full z-50 h-full px-6 relative max-w-4xl'>
                         <div className='w-full h-full flex items-center justify-center relative'>
                             <div className='w-full relative'>
-                                <IoClose
-                                    fontSize={"35"}
-                                    className='cursor-pointer absolute -top-16 right-0'
-                                    onClick={closeModal}
-                                />
-                                <YouTube
-                                    videoId={youtubePlayer.videoId}
-                                    loading='lazy'
-                                    iframeClassName='w-full min-h-[500px]'
-                                />
+                                <IoClose fontSize={"35"} className='cursor-pointer absolute -top-16 right-0' onClick={closeModal} />
+                                <YouTube videoId={youtubePlayer.videoId} loading='lazy' iframeClassName='w-full min-h-[500px]' />
                             </div>
                         </div>
                     </div>
