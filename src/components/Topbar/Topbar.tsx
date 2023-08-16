652421f30db482ba6a78532fc88b8faaab163ee8
safchik <safchik9@gmail.com>
2023-08-16 12:18:55 +0100

    topbar auth updated, logout functionality and reset password added

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import Image from "next/image";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/firebase";
+import Logout from "../Buttons/Logout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
 
 
 type TopbarProps = {
@@ -12,7 +15,7 @@ type TopbarProps = {
 
 const Topbar: React.FC<TopbarProps> = () => {
     const [user] = useAuthState(auth);
-
+    const setAuthModalState = useSetRecoilState(authModalState);
 
     return (
         <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
@@ -29,7 +32,9 @@ const Topbar: React.FC<TopbarProps> = () => {
                         </a>
                     </div>
                     {!user && (
-                        <Link href='/auth'>
+                        <Link href='/auth' onClick={() => {
+                            setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }));
+                        }}>
                             <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2 duration-200">Sign In</button>
                         </Link>
                     )}
@@ -45,6 +50,7 @@ const Topbar: React.FC<TopbarProps> = () => {
                             </div>
                         </div>
                     )}
+                    {user && <Logout />}
                 </div>
             </div>
         </nav>
