a546d9b89393a559787445dc374a87940301332f
safchik <safchik9@gmail.com>
2023-08-16 19:29:52 +0100

    problems with branches

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilValue } from 'recoil';
-import Image from 'next/image';
 
 type AuthPageProps = {};
 
@@ -28,7 +27,7 @@ const AuthPage: React.FC<AuthPageProps> = () => {
         <div className="max-w-7xl mx-auto">
             <Navbar />
             <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
-                <Image src='/hero.png' alt='Hero img' width={700} height={700} />
+                <img src='/hero.png' alt='Hero img' />
             </div>
             {authModal.isOpen && <AuthModal />}
         </div>
